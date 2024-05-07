16f0b56d51744d590558f127029711fb4a60eca6
ej57750 <147657192+ej57750@users.noreply.github.com>
2024-05-07 09:33:07 +0200

    Add files via upload
    First version of SAT solver

---
+++ main.py
@@ -0,0 +1,97 @@
+
+with open('sudoku_mini.txt') as d:
+    lines = d.read().splitlines()
+
+info_row = lines[2].split(" ")
+no_variables = int(info_row[2])
+no_rows = int(info_row[3])
+
+clauses = []
+
+for i in range(3, no_rows + 1):
+
+    clause = lines[i].split(" ")[:-2]
+    actual_clause = [0 for k in range(no_variables + 1)]
+
+    for j in range(len(clause)):
+        number = int(clause[j])
+        if number > 0:
+            actual_clause[number] = 1
+        else:
+            actual_clause[-1 * number] = -1
+
+    clauses.append(actual_clause)
+
+def find_variables(clauses, final_variables):
+
+    while len(clauses) > 0:
+
+        flag = False
+
+        for cl in range(len(clauses)):
+
+            if len(clauses[cl]) == 1:
+
+                value = clauses[cl][0]
+                final_variables[cl] = clauses[cl][0]
+                clauses = clauses[:cl] + clauses[cl+1:]
+
+                flag = True
+
+                for a in range(len(clauses)):
+
+                    acl = clauses[a]
+
+                    if acl[cl] == value:
+                        clauses = clauses[:a] + clauses[a + 1:]
+                    elif acl[cl] == -1 * value:
+                        acl[cl] = 0
+                        clauses[a] = acl
+
+            elif not (1 in clauses[cl] or -1 in clauses[cl]):
+
+                return clauses, final_variables, False
+
+        if not flag:
+
+            for g in range(len(clauses)):
+
+                for h in range(no_variables):
+
+                    var_no = h
+                    var_val = clauses[g][h]
+
+                    if var_val == 0:
+                        continue
+
+                    temp_clauses = clauses.copy()
+                    temp_variables = final_variables.copy()
+
+                    temp_variables[var_no] = var_val
+                    temp_clauses = temp_clauses[1:]
+
+                    for a in range(len(temp_clauses)):
+
+                        acl = temp_clauses[a]
+
+                        if acl[var_no] == var_val:
+                            temp_clauses = temp_clauses[:a] + temp_clauses[a + 1:]
+                        elif acl[var_no] == -1 * var_val:
+                            acl[var_no] = 0
+                            temp_clauses[a] = acl
+
+                    tcl, tvar, found = find_variables(temp_clauses, temp_variables)
+
+                    if found:
+                        return tcl, tvar, True
+
+            return clauses, final_variables, False
+
+
+variables = [0 for var in range(no_variables + 1)]
+
+final_variables = find_variables(clauses, variables)
+
+print(final_variables)
+
+
