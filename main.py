6acb36533dfac1bc35e9c194863e08c125da98ea
ej57750 <147657192+ej57750@users.noreply.github.com>
2024-05-17 12:14:46 +0200

    Add files via upload
    Updated version, this one should work, just have to check a few things

--- main.py
+++ main.py
@@ -1,112 +1,129 @@
 
-with open('test.txt') as d:
-    lines = d.read().splitlines()
+def find_variables(clauses, variables):
+
+    number_of_clauses = len(clauses)
+    number_of_variables = len(variables)
+
+    if number_of_clauses == 0:
+        return clauses, variables, True
+
+    for cl in range(number_of_clauses):
+
+        clause = clauses[cl]
+        check = [clause[ch] for ch in range(len(clause)) if ch != 0]
+
+        if len(check) == 1:
+
+            index = int(check[0])
+            value = clause[index]
+
+            if variables[index] == -1 * value:
+                return clauses, variables, False
+
+            variables[index] = value
+
+            clauses = clauses[:cl] + clauses[cl+1:]
+
+            number_of_clauses -= 1
+
+            delete = []
+
+            for a in range(number_of_clauses):
+
+                acl = clauses[a]
+
+                if acl[index] == value:
+                    delete.append(a)
+                elif acl[index] == -1 * value:
+                    clauses[a][index] = 0
+                    if all(val == 0 for val in clauses[a]):
+                        return clauses, variables, False
+
+            clauses = [clauses[clause_no] for clause_no in range(number_of_clauses) if clause_no not in delete]
+
+            return variables(clauses, variables)
+
+    for clause_no in range(number_of_clauses):
+        for var_no in range(number_of_variables):
+
+            var_val = clauses[clause_no][var_no]
+
+            if var_val == 0:
+                continue
+
+            temp_clauses = clauses.copy()
+            temp_variables = variables.copy()
+
+            temp_variables[var_no] = var_val
+            temp_clauses = temp_clauses[:clause_no] + temp_clauses[clause_no+1:]
+
+            no_temp_clauses = number_of_clauses - 1
+
+            incorrect = False
+            delete = []
+
+            for a in range(no_temp_clauses):
+
+                acl = temp_clauses[a]
+
+                if acl[var_no] == var_val:
+                    delete.append(a)
+                elif acl[var_no] == -1 * var_val:
+                    temp_clauses[a][var_no] = 0
+                    if all(val == 0 for val in temp_clauses[a]):
+                        incorrect = True
+                        break
+
+            if incorrect:
+                continue
+
+            temp_clauses = [temp_clauses[x] for x in range(len(temp_clauses)) if x not in delete]
+
+            tcl, tvar, found = find_variables(temp_clauses, temp_variables)
+
+            if found:
+                return tcl, tvar, found
+
+    return clauses, variables, False
+
+
+name = "test"
+task_name = name + ".txt"
+solution_name = name + "_solution.txt"
+
+with open(task_name) as data:
+    lines = data.read().splitlines()
 
 info_row = lines[2].split(" ")
-no_variables = int(info_row[2])
+no_columns = int(info_row[2])
 no_rows = int(info_row[3])
 
-clauses = []
+starting_clauses = []
 
-for i in range(3, no_rows + 3):
+for i in range(3, no_columns + 3):
 
-    clause = lines[i].split(" ")[:-1]
-    actual_clause = [0 for k in range(no_variables + 1)]
+    one_clause = lines[i].split(" ")[:-1]
+    clause_with_values = [0 for k in range(no_columns + 1)]
 
-    for j in range(len(clause)):
-        number = int(clause[j])
+    for j in range(one_clause):
+        number = int(one_clause[j])
         if number > 0:
-            actual_clause[number] = 1
+            clause_with_values[number] = 1
         else:
-            actual_clause[-1 * number] = -1
+            clause_with_values[-1 * number] = -1
 
-    clauses.append(actual_clause)
-
-print(clauses)
+    starting_clauses.append(clause_with_values)
 
 
-def find_variables(clauses, final_variables):
+starting_variables = [0 for var in range(no_columns + 1)]
 
-    while len(clauses) > 0:
+final_clauses, final_variables, found_solution = find_variables(starting_clauses, starting_variables)
 
-        flag = False
-
-        for cl in range(len(clauses)):
-
-            if len(clauses[cl]) == 1:
-
-                value = clauses[cl][0]
-                final_variables[cl] = clauses[cl][0]
-                clauses = clauses[:cl] + clauses[cl+1:]
-
-                flag = True
-
-                # fix how we delete the unneeded clauses
-
-                delete = []
-
-                for a in range(len(clauses)):
-
-                    acl = clauses[a]
-
-                    if acl[cl] == value:
-                        delete.append(a)
-                        #clauses = clauses[:a] + clauses[a + 1:]
-                    elif acl[cl] == -1 * value:
-                        acl[cl] = 0
-                        clauses[a] = acl
-
-                clauses = [clauses[x] for x in range(len(clauses)) if x not in delete]
-
-            elif not (1 in clauses[cl] or -1 in clauses[cl]):
-
-                return clauses, final_variables, False
-
-        if not flag:
-
-            for g in range(len(clauses)):
-
-                for h in range(no_variables):
-
-                    var_no = h
-                    var_val = clauses[g][h]
-
-                    if var_val == 0:
-                        continue
-
-                    temp_clauses = clauses.copy()
-                    temp_variables = final_variables.copy()
-
-                    temp_variables[var_no] = var_val
-                    temp_clauses = temp_clauses[1:]
-
-                    delete = []
-
-                    for a in range(len(temp_clauses)):
-
-                        acl = temp_clauses[a]
-
-                        if acl[var_no] == var_val:
-                            delete.append(a)
-                            #temp_clauses = temp_clauses[:a] + temp_clauses[a + 1:]
-                        elif acl[var_no] == -1 * var_val:
-                            acl[var_no] = 0
-                            temp_clauses[a] = acl
-
-                    temp_clauses = [temp_clauses[x] for x in range(len(temp_clauses)) if x not in delete]
-
-                    tcl, tvar, found = find_variables(temp_clauses, temp_variables)
-
-                    if found:
-                        return tcl, tvar, True
-
-            return clauses, final_variables, False
-
-
-variables = [0 for var in range(no_variables + 1)]
-
-final_variables = find_variables(clauses, variables)
-
-print(final_variables)
-
+if found_solution:
+    variables_index_values = [str(index * final_variables[index]) for index in
+                              range(len(final_variables)) if final_variables[index] != 0]
+    variables_string = " ".join(variables_index_values)
+    print(variables_string)
+else:
+    print(False)
 
