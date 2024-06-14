56217f3b08690d25cd49faa97bca47d0abe2ef1a
ej57750 <147657192+ej57750@users.noreply.github.com>
2024-06-14 19:17:54 +0200

    Add files via upload
    2.3 and 2.5

--- mainproject.lean
+++ mainproject.lean
@@ -1,28 +1,41 @@
 
+import Mathlib
+
 open Nat
+open Set
 
 -- Task 1.1: Formalize the recursive definition of the catalan numbers.
 
 mutual
 
-  def mysum (i : Nat) (n : Nat) (x : Nat -> Nat) :=
+  def my_sum (i : Nat) (n : Nat) (x : Nat -> Nat) :=
     if i = 0 then x 0 * x n
-    else x i * x (n - i) + mysum (i - 1) n x
+    else x i * x (n - i) + my_sum (i - 1) n x
 
-  def mainsum (n : Nat) (x : Nat -> Nat) :=
-    mysum n n x
+  def main_sum (n : Nat) (x : Nat -> Nat) :=
+    my_sum n n x
 
-  def catalan (n : Nat) :=
+  def my_catalan (n : Nat) :=
     if n = 0 then 1
-    else mainsum (n - 1) catalan
+    else main_sum (n - 1) my_catalan
 
 end
+
+#eval my_catalan 0  --1
+#eval my_catalan 1  --1
+#eval my_catalan 2  --2
+#eval my_catalan 3  --5
+#eval my_catalan 4  --14
 
 -- Task 1.2: Formalize the concept of plane trees.
 
 inductive plane_tree
 | leaf
 | node : List plane_tree -> plane_tree
+deriving Repr
+
+#check plane_tree.leaf
+#eval plane_tree.node [plane_tree.leaf, plane_tree.leaf]
 
 -- Task 1.3: Formalize the concept of full binary trees.
 
@@ -62,7 +75,24 @@ def valid_ballot_sequence : Type := { votes : List vote // valid_sequence votes 
 -- Task 2.2: Construct a bijection between full binary trees and
 --            the type Fin(Cn).
 
--- Task 2.3: Construct a bijection.
+-- Task 2.3: Construct a bijection between lists of plane trees and
+--            plane trees.
+
+def plane_tree_to_list (T : plane_tree) : List plane_tree :=
+  match T with
+  | plane_tree.leaf => []
+  | plane_tree.node Ts => Ts
+
+#eval plane_tree_to_list plane_tree.leaf
+#eval plane_tree_to_list (plane_tree.node [plane_tree.leaf, plane_tree.leaf])
+
+def plane_tree_from_list (Ts : List plane_tree) : plane_tree :=
+  match Ts with
+  | [] => plane_tree.leaf
+  | _ => plane_tree.node Ts
+
+#eval plane_tree_from_list []
+#eval plane_tree_from_list [plane_tree.leaf, plane_tree.leaf]
 
 -- Task 2.4: Construct the rotating isomorphism, which is the
 --            isomorphism between plane trees and full binary trees.
@@ -70,5 +100,12 @@ def valid_ballot_sequence : Type := { votes : List vote // valid_sequence votes 
 -- Task 2.5: Prove that Bin(2n, n) is divisible by n + 1.
 
 -- Task 2.6: Prove that C_n = 1 / (n + 1) * Bin(2n, n).
+
+theorem explicit_catalan (n : Nat) : Nat :=
+  (Nat.choose (2 * n) n) / (n + 1)
+
+theorem binomial_divisibility (n : ℕ) : (Nat.choose (2 * n) n) % (n + 1) = 0 := by
+  have h : (n + 1) * explicit_catalan n = Nat.choose (2 * n) n
+
 
 -- Task 2.7: Construct an isomorphism.
